659af81a3c4b276208c73d35c6e4e6fefbbefeb3
Kaixi26 <jorgejg575926@gmail.com>
2020-12-08 23:01:19 +0000

    started refactoring

---
+++ ats_50_refactored/src/main/java/StringDistAux.java
@@ -0,0 +1,40 @@
+import java.io.Serializable;
+
+/**
+ * Classe auxiliar que agrupa um determinado código a uma distância
+ */
+public class StringDistAux implements Serializable {
+    public double dist;
+    public String cod;
+
+    public StringDistAux(double dist, String cod) {
+        this.dist = dist;
+        this.cod = cod;
+    }
+
+    public StringDistAux(StringDistAux a) {
+        this.dist = a.getDist();
+        this.cod = a.getCod();
+    }
+
+    public double getDist() {
+        return dist;
+    }
+
+    public void setDist(double dist) {
+        this.dist = dist;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public StringDistAux clone() {
+        return new StringDistAux(this);
+    }
+
+}

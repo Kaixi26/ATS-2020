adf5b084945f58785ef5905fabfc21c4cb7b2a72
Kaixi26 <jorgejg575926@gmail.com>
2021-01-24 12:56:22 +0000

    added consumption graphs

--- scripts/consumption_generator.sh
+++ scripts/consumption_generator.sh
@@ -3,7 +3,7 @@
 for i in 10 100 1000 10000 20000 50000
 do
     pushd logGenerator
-    stack --allow-different-user run -- --users $i --volunteers $i --transport $i --stores $i --orders $i --accepted $i\
+    stack --allow-different-user run -- logGenerator --users $i --volunteers $i --transport $i --stores $i --orders $i --accepted $i\
         > ../files/Logs.txt
     popd
     for n in {0..19}
@@ -14,7 +14,7 @@ do
             (sudo java -cp ats_$proj\_refactored/target/classes/ EnergyCheckUtils $i $i $i $i $i | tail -1)\
                 >> consumption_tests/$proj\_$i
             (sudo java -cp ats_$proj\_refactored/target/classes/ EnergyCheckUtils -1 | tail -1)\
-                >> consumption_tests/50_logs_$i
+                >> consumption_tests/$proj\_logs_$i
         done
     done
 done
